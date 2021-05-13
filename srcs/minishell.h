51736c427ad4b6e7bd2daa1af63896d7937013da
scass <scass@student.21-school.ru>
2021-05-13 12:05:50 -0700

    f

---
+++ srcs/minishell.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: scass <scass@student.21-school.ru>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/13 02:15:21 by scass             #+#    #+#             */
+/*   Updated: 2021/05/13 02:17:27 by scass            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <string.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+# include <sys/types.h>
+
+
+
+#endif
