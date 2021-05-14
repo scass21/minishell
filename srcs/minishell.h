9a0659f927612a542894339eccdc2a999a7bee50
Дарья Паркина <lkasandr@MacBook-Pro-Dara.local>
2021-05-14 21:06:03 +0300

    11111111111

--- srcs/minishell.h
+++ srcs/minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: scass <scass@student.21-school.ru>         +#+  +:+       +#+        */
+/*   By: lkasandr <lkasandr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/13 02:15:21 by scass             #+#    #+#             */
-/*   Updated: 2021/05/13 02:17:27 by scass            ###   ########.fr       */
+/*   Updated: 2021/05/14 21:05:28 by lkasandr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@
 # include <sys/stat.h>
 # include <fcntl.h>
 # include <sys/types.h>
+
 
 
 
